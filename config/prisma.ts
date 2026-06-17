7ce2422dd96fa9c0e5200bf07e87b5a5484899f7
Giovanni Donati <grdonati99@gmail.com>
2026-06-17 07:35:01 -0300

    feat: inicial migration, with user table

--- config/prisma.ts
+++ config/prisma.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../src/generated/prisma";
 dotenv.config();
 
 const connectionString = `${process.env.DATABASE_URL}`;

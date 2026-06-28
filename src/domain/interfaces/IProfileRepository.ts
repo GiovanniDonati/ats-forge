657e6159537285a6148a1b3242781e564435371e
GiovanniDonati <grdonati99@gmail.com>
2026-06-28 20:27:28 -0300

    feat: update profile repository interface

--- src/domain/interfaces/IProfileRepository.ts
+++ src/domain/interfaces/IProfileRepository.ts
@@ -1,8 +1,9 @@
-import { ProfileEntity } from '../entities/Profile';
+import { ProfileEntity } from "../entities/profile/Profile";
 
 export interface IProfileRepository {
   create(profile: ProfileEntity): Promise<ProfileEntity>;
   findById(id: string): Promise<ProfileEntity | null>;
+  findAll(): Promise<ProfileEntity[]>;
   findByUserId(userId: string): Promise<ProfileEntity[]>;
   update(id: string, profile: Partial<ProfileEntity>): Promise<ProfileEntity>;
   delete(id: string): Promise<void>;

0834ece0728016c1735b39556e5646efb66cb21e
Giovanni Donati <grdonati99@gmail.com>
2026-07-06 12:25:13 -0300

    refactor: Update the main file, to use express

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { run } from './presentation/cli/CliController';
+import express from 'express';
+import ProfileRoutes from './presentation/routes/ProfileRoutes';
+import UserRoutes from './presentation/routes/UserRoutes';
+import AuthRoutes from './presentation/routes/AuthRoutes';
 
-run(process.argv).catch((err: Error) => {
-  console.error(`[erro] ${err.message}`);
-  process.exit(1);
+const app = express();
+app.use(express.json());
+
+app.use('/auth', AuthRoutes);
+app.use('/profiles', ProfileRoutes);
+app.use('/users', UserRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
